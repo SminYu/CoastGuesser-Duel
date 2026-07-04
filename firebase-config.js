314de36d10f908ab85385537c21a53b0b86c2f32
Seungmin Yu <70931273+SminYu@users.noreply.github.com>
2026-07-04 12:55:00 +0900

    Refresh Firebase config cache

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.COAST_DUEL_FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAaOBFQGLSGqnAp0m7wqovJYmmY1cK0DCk",
-    authDomain: "coastguesser-duel.firebaseapp.com",
-    projectId: "coastguesser-duel",
-    storageBucket: "coastguesser-duel.firebasestorage.app",
-    messagingSenderId: "608797382783",
-    appId: "1:608797382783:web:19bba7d0759d0d7c9b074a"
+  apiKey: "AIzaSyAaOBFQGLSGqnAp0m7wqovJYmmY1cK0DCk",
+  authDomain: "coastguesser-duel.firebaseapp.com",
+  projectId: "coastguesser-duel",
+  storageBucket: "coastguesser-duel.firebasestorage.app",
+  messagingSenderId: "608797382783",
+  appId: "1:608797382783:web:19bba7d0759d0d7c9b074a"
 };
