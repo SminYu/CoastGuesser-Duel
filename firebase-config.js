0b29781d92054b16141a67320358fe3cbac69c88
Seungmin Yu <70931273+SminYu@users.noreply.github.com>
2026-07-04 12:48:14 +0900

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.COAST_DUEL_FIREBASE_CONFIG = {
-  apiKey: "PASTE_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID"
+    apiKey: "AIzaSyAaOBFQGLSGqnAp0m7wqovJYmmY1cK0DCk",
+    authDomain: "coastguesser-duel.firebaseapp.com",
+    projectId: "coastguesser-duel",
+    storageBucket: "coastguesser-duel.firebasestorage.app",
+    messagingSenderId: "608797382783",
+    appId: "1:608797382783:web:19bba7d0759d0d7c9b074a"
 };
